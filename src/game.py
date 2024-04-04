048f5be0dedc8b8dc60d902f86d090890ae52438
oSmartCoder <92610222+oSmartCoder@users.noreply.github.com>
2024-04-05 00:18:49 +1100

    Update game.py

--- src/game.py
+++ src/game.py
@@ -2,6 +2,7 @@ import pygame
 from pytmx.util_pygame import load_pygame
 
 from .player import Player
+from .tile import Tile
 from settings import *
 
 class Game:
@@ -14,11 +15,32 @@ class Game:
         Player((400, 400), self.visible_sprites)
 
 
+        self.load_level()
 
-    def background(self):
-        self.win.fill((0, 0, 0))
+    def load_level(self):
+        
+        tmx_data = load_pygame('./assets/tmx/level_1.tmx')
+
+        # print(tmx_data.width, tmx_data.tilewidth, tmx_data.height, tmx_data.tileheight)
+
+
+        for layer in tmx_data.visible_layers:
+            if hasattr(layer, 'data'):
+                for x, y, surface in layer.tiles():
+                    pos = (x * TILE_SIZE, y * TILE_SIZE)
+
+                    image = pygame.transform.scale(surface, (TILE_SIZE, TILE_SIZE))
+                    Tile(pos, image, self.visible_sprites)
+                
+
+
+        
+
 
     def update(self):
-        self.background()
+        self.win.fill((37, 19, 26))
+
+        self.visible_sprites.draw(self.win)
         self.visible_sprites.update()
+
 
