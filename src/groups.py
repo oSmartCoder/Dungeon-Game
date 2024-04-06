a87ab466741d03d27ad07ae47d9a04e7bd5f512c
oSmartCoder <92610222+oSmartCoder@users.noreply.github.com>
2024-04-07 00:03:57 +1100

    Update groups.py

--- src/groups.py
+++ src/groups.py
@@ -19,31 +19,18 @@ class CameraGroup(pygame.sprite.Group):
     def custom_draw(self, player: pygame.sprite.Sprite):
         self.center_target_camera(player)
 
+
+
         for sprite in self.sprites():
+            
             offset_pos = sprite.rect.topleft - self.offset
+
             self.win.blit(sprite.image, offset_pos)
-
-
-class CollisionGroup(pygame.sprite.Group):
-    def __init__(self):
-        super().__init__()
-
-        self.win = pygame.display.get_surface()
-
-    
-    def collision_update(self, active_sprites: pygame.sprite.Group):
-        for active_sprite in active_sprites.sprites():
-            for collision_sprite in self.sprites():
-                if active_sprite.rect.colliderect(collision_sprite.rect):
-                    if active_sprite.direction.x > 0:
-                        active_sprite.rect.right = collision_sprite.rect.left
-                    elif active_sprite.direction.x < 0:
-                        active_sprite.rect.left = collision_sprite.rect.right
-                
-                    if active_sprite.direction.y > 0:
-                        active_sprite.rect.bottom = collision_sprite.rect.top
-                    elif active_sprite.direction.y < 0:
-                        active_sprite.rect.top = collision_sprite.rect.bottom
+            
+        self.win.blit(player.animations['shadow'], player.rect.midleft - self.offset + Vector2(4, 20))
+        
+                        
+                    
                         
 
 
