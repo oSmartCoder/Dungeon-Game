04f4ad9eb205cb5adfddf890d40d93468ad786f4
oSmartCoder <92610222+oSmartCoder@users.noreply.github.com>
2024-04-07 00:03:55 +1100

    Update tile.py

--- src/tile.py
+++ src/tile.py
@@ -1,8 +1,16 @@
 import pygame
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self, pos, surface, *groups):
-        super().__init__(*groups)
+    def __init__(self, pos, surface, groups, layer_name):
+        super().__init__(groups)
 
         self.image = surface
         self.rect = self.image.get_rect(topleft = pos)
+        self.layer_name = layer_name
+
+
+class AnimatedTile(Tile):
+    def __init__(self, pos, surface, groups, layer_name):
+        super().__init__(pos, surface, groups, layer_name)
+        
+
