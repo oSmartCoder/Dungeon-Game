a06645437a6b4da5140b960d962bca4d826b5405
oSmartCoder <92610222+oSmartCoder@users.noreply.github.com>
2024-04-07 00:03:57 +1100

    Update game.py

--- src/game.py
+++ src/game.py
@@ -1,9 +1,10 @@
 import pygame
+from pytmx import TiledTileLayer
 from pytmx.util_pygame import load_pygame
 
 from .player import Player
 from .tile import Tile
-from .groups import CameraGroup, CollisionGroup
+from .groups import CameraGroup
 
 from settings import *
 
@@ -15,7 +16,7 @@ class Game:
 
         self.camera_sprites = CameraGroup()
         self.active_sprites = pygame.sprite.Group()
-        self.collision_sprites = CollisionGroup()
+        self.collision_sprites = pygame.sprite.Group()
         self.moveable_sprites = pygame.sprite.Group()
 
         self.load_level()
@@ -24,34 +25,43 @@ class Game:
         
         tmx_data = load_pygame('./assets/tmx/level_1.tmx')
 
+
+
         for layer in tmx_data.visible_layers:
-            if hasattr(layer, 'data'):
+            if isinstance(layer, TiledTileLayer):
+
                 for x, y, surface in layer.tiles():
+
+                    print(tmx_data.get_tile_)
+                    
                     pos = (x * TILE_SIZE, y * TILE_SIZE)
                     image = pygame.transform.scale(surface, (TILE_SIZE, TILE_SIZE))
 
+                    # Priority Order
                     if layer.name == 'Ground' or layer.name == 'Non-Collidable Entities':
-                        Tile(pos, image, self.camera_sprites)
+                        Tile(pos, image, self.camera_sprites, layer.name)
 
                     elif layer.name == 'Collidable Entities':
-                        Tile(pos, image, self.camera_sprites, self.collision_sprites)
+                        Tile(pos, image, [self.camera_sprites, self.collision_sprites], layer.name)
 
                     elif layer.name == 'Moveable Entities':
-                        Tile(pos, image, self.camera_sprites, self.moveable_sprites)
+                        Tile(pos, image, [self.camera_sprites, self.moveable_sprites], layer.name)
           
 
         pos = (obj:=tmx_data.get_object_by_name('Spawn Point')).x / tmx_data.tilewidth * TILE_SIZE, obj.y / tmx_data.tileheight * TILE_SIZE
 
-        Player(pos, self.camera_sprites, self.active_sprites)
+        Player(pos, [self.camera_sprites, self.active_sprites], self.collision_sprites)
         
 
     def update(self):
         self.win.fill((37, 19, 26))
 
-        self.camera_sprites.update()
+        
+    
         self.camera_sprites.custom_draw(self.active_sprites.sprites()[0])
+        
+        self.camera_sprites.update()
 
-        self.collision_sprites.collision_update(self.active_sprites)
         
 
 
