5a6c07627feab1fc80592ddea0266524ec4d1cf2
oSmartCoder <92610222+oSmartCoder@users.noreply.github.com>
2024-04-09 23:21:30 +1000

    Update game.py

--- src/game.py
+++ src/game.py
@@ -3,9 +3,8 @@ from pytmx import TiledTileLayer
 from pytmx.util_pygame import load_pygame
 
 from .player import Player
-from .tile import Tile
-from .groups import CameraGroup
-
+from .tile import *
+from .groups import CameraGroup, AnimationGroup, InteractiveGroup
 from settings import *
 
 
@@ -13,59 +12,73 @@ class Game:
     def __init__(self):
         self.win = pygame.display.get_surface()
 
-
-        self.camera_sprites = CameraGroup()
+        
         self.active_sprites = pygame.sprite.Group()
         self.collision_sprites = pygame.sprite.Group()
-        self.moveable_sprites = pygame.sprite.Group()
+
+        self.camera_sprites = CameraGroup()
+        self.interactive_sprites = InteractiveGroup()
+        self.animation_sprites = AnimationGroup()
 
         self.load_level()
 
     def load_level(self):
-        
         tmx_data = load_pygame('./assets/tmx/level_1.tmx')
 
-
-        print(dir(tmx_data))
-
-        print(tmx_data)
-
-        for i, layer in enumerate(tmx_data.visible_layers):
-
-            
+        for layer in tmx_data.visible_layers:
             if isinstance(layer, TiledTileLayer):
                 for x, y, surface in layer.tiles():
-
-                    
-                    
                     pos = (x * TILE_SIZE, y * TILE_SIZE)
                     image = pygame.transform.scale(surface, (TILE_SIZE, TILE_SIZE))
 
-                    # Priority Order
                     match layer.name:
-                        case 'Ground' | 'Bones' | 'Torches' | 'Flags':
+                        case 'Ground' | 'Bone':
                             Tile(pos, image, self.camera_sprites, layer.name)
                         
-                        case 'Walls' | 'Chest':
+                        case 'Wall':
                             Tile(pos, image, [self.camera_sprites, self.collision_sprites], layer.name)
+                        
+                        case 'Chest' | 'Mini Chest':
+                            Chest(pos, image, [self.camera_sprites, self.collision_sprites, self.animation_sprites, self.interactive_sprites], layer.name)
 
-                        case 'Doors':
-                            Tile(pos, image, [self.camera_sprites, self.moveable_sprites], layer.name)
-          
+                        case 'Small Red Flask' | 'Large Red Flask' | 'Small Blue Flask' | 'Large Blue Flask':
+                            Flask(pos, image, [self.camera_sprites, self.animation_sprites, self.interactive_sprites], layer.name)
 
-        pos = (obj:=tmx_data.get_object_by_name('Spawn Point')).x / tmx_data.tilewidth * TILE_SIZE, obj.y / tmx_data.tileheight * TILE_SIZE
+                        case 'Flag':
+                            Flag(pos, image, [self.camera_sprites, self.animation_sprites], layer.name)
 
-        Player(pos, [self.camera_sprites, self.active_sprites], self.collision_sprites)
+                        case 'Front Torch' | 'Right Torch' | 'Left Torch' | 'Small Candlestick' | 'Tall Candlestick':
+                            Torch(pos, image, [self.camera_sprites, self.animation_sprites], layer.name)
+
+                        case 'Coin':
+                            Coin(pos, image, [self.camera_sprites, self.animation_sprites, self.interactive_sprites], layer.name)
+                        
+                        case 'Ladder':
+                            Ladder(pos, image, [self.camera_sprites, self.interactive_sprites], layer.name)
+                        
+                        case 'Golden Key' | 'Silver Key':
+                            Key(pos, image, [self.camera_sprites, self.animation_sprites, self.interactive_sprites], layer.name)
+                        
+                        case 'Mini Brown Box' | 'Mini Silver Box' | 'Brown Box' | 'Silver Box':
+                            Box(pos, image, [self.camera_sprites, self.animation_sprites, self.interactive_sprites], layer.name)
+        
+                        case 'Door':
+                            Door(pos, image, [self.camera_sprites, self.interactive_sprites], layer.name)
+
+                        case _:
+                            raise ValueError(f'Cannot assign layer \'{layer.name}\' to associated class')
+
+        spawn_point_pos = (obj:=tmx_data.get_object_by_name('Spawn Point')).x / tmx_data.tilewidth * TILE_SIZE, obj.y / tmx_data.tileheight * TILE_SIZE
+
+        Player(spawn_point_pos, [self.camera_sprites, self.active_sprites], self.collision_sprites)
         
 
     def update(self):
         self.win.fill((37, 19, 26))
 
-        
+        self.animation_sprites.animate()
     
-        self.camera_sprites.custom_draw(self.active_sprites.sprites()[0])
-        
-        self.camera_sprites.update()
+        self.camera_sprites.custom_update(self.active_sprites.sprites()[0])
 
         
 
