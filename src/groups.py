dc2580b432614633f48d43c7267d542e2fa18a3e
oSmartCoder <92610222+oSmartCoder@users.noreply.github.com>
2024-04-09 23:21:28 +1000

    Update groups.py

--- src/groups.py
+++ src/groups.py
@@ -16,18 +16,50 @@ class CameraGroup(pygame.sprite.Group):
         self.offset.y = target.rect.centery - WIN_Y / 2
 
     
-    def custom_draw(self, player: pygame.sprite.Sprite):
+    def custom_update(self, player: pygame.sprite.Sprite):
         self.center_target_camera(player)
-
-
 
         for sprite in self.sprites():
             
             offset_pos = sprite.rect.topleft - self.offset
-
-            self.win.blit(sprite.image, offset_pos)
             
-        self.win.blit(player.animations['shadow'], player.rect.midleft - self.offset + Vector2(3, 20))
+            if sprite != player:
+                self.win.blit(sprite.image, offset_pos)
+        
+        player.update(self.offset)
+
+
+class AnimationGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+
+        self.win = pygame.display.get_surface()
+
+    
+    def animate(self):
+        for sprite in self.sprites():
+
+            sprite.animation_index += sprite.animation_speed
+
+            if sprite.animation_index >= len(sprite.animations):
+                sprite.animation_index = 0
+
+            sprite.image = sprite.animations[int(sprite.animation_index)]
+
+
+
+class InteractiveGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+
+        self.win = pygame.display.get_surface()
+
+    
+    def update(self):
+        pass
+
+            
+        
         
                         
                     
