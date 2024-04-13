1243c63657c9e49e6a71f0a651657f995415811a
oSmartCoder <92610222+oSmartCoder@users.noreply.github.com>
2024-04-13 23:23:52 +1000

    Update Dungeon Tileset.tsx

--- assets/tsx/Dungeon Tileset.tsx
+++ assets/tsx/Dungeon Tileset.tsx
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="Dungeon Tileset" tilewidth="16" tileheight="16" tilecount="100" columns="10">
  <image source="../character and tileset/Dungeon_Tileset.png" width="160" height="160"/>
+ <tile id="23" probability="20"/>
 </tileset>
