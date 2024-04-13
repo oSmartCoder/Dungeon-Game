642b9036047c357c51a9bedd5553eb636300a235
oSmartCoder <92610222+oSmartCoder@users.noreply.github.com>
2024-04-13 23:23:35 +1000

    Update enemy.py

--- src/enemy.py
+++ src/enemy.py
@@ -11,7 +11,7 @@ from settings import *
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, pos: Tuple[float, float], groups: list, enemy_name: str):
         """
-        Note: Parent classes of Enemy (groups) access delta, image, and rect for external calculation and attribute reassignment
+        Note: Parent classes of Enemy (groups) access delta, direction, image, and rect for external calculation and value modification
         """
         super().__init__(groups)
         self.enemy_name = enemy_name
@@ -31,6 +31,11 @@ class Enemy(pygame.sprite.Sprite):
         self.delta = Vector2()
         
         # Enemy properties
+        self.disable_pursue = False
+        self.triggered_delay = False
+        self.delay = 5
+        self.counter = 0
+        self.facing_right = True
         self.image = self.animations[int(self.animation_index)]
         self.rect = self.image.get_rect(topleft=pos)
 
@@ -51,26 +56,48 @@ class Enemy(pygame.sprite.Sprite):
 
     def persue_player(self, player):
         self.delta = Vector2(self.rect.center).move_towards(player.rect.center, self.vel) - Vector2(self.rect.center)
+    
+    def trigger_delay(self):
+        if self.triggered_delay:
+            if self.counter > self.delay:
+                self.triggered_delay = False 
+                self.disable_persue = False
+                self.counter = 0
+            else:
+                self.counter += 0.1
+                self.delta = Vector2()
 
+
+    def update_image(self):
         image = self.image
 
-        if self.delta.x > 0:
+        if self.facing_right:
             self.image = image
-            self.direction.x = 1
         else:
             self.image = pygame.transform.flip(image, True, False)
+
+    def update_direction(self):
+        if self.delta.x > 0:
+            self.facing_right = True 
+            self.direction.x = 1
+        else:
+            self.facing_right = False
             self.direction.x = -1
         
         if self.delta.y > 0:
             self.direction.y = 1
         else:
             self.direction.y = -1
-    
+
     def update(self, player: pygame.sprite.Sprite):
-        if Vector2(player.rect.center).distance_to(self.rect.center) <= self.data['pursue radius']:
+        if Vector2(player.rect.center).distance_to(self.rect.center) <= self.data['pursue radius'] and not self.disable_pursue:
             self.persue_player(player)
+            self.update_direction()
         else:
             self.delta = Vector2()
+        
+        self.trigger_delay()
+        self.update_image()
 
 
 
