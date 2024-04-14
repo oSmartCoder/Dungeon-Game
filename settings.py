43851a0db6de787e163f0aedf37381d26941bed1
oSmartCoder <92610222+oSmartCoder@users.noreply.github.com>
2024-04-14 23:19:50 +1000

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,3 +3,10 @@ FPS = 60
 TILE_SIZE = 64
 
 SIZE = WIN_X, WIN_Y = 1200, 700
+
+COLOURS = {
+    'background': (37, 19, 26),
+    'red': (215, 59, 74),
+    'orange': (255, 188, 63),
+    'green': (67, 186, 64)
+}
