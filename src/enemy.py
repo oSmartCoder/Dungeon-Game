3dbfcfba7baf18667260574d1250e51d771e60a8
oSmartCoder <92610222+oSmartCoder@users.noreply.github.com>
2024-04-14 23:19:52 +1000

    Update enemy.py

--- src/enemy.py
+++ src/enemy.py
@@ -7,13 +7,15 @@ from os import listdir
 
 from settings import *
 
-
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, pos: Tuple[float, float], groups: list, enemy_name: str):
         """
         Note: Parent classes of Enemy (groups) access delta, direction, image, and rect for external calculation and value modification
         """
         super().__init__(groups)
+        
+        self.win = pygame.display.get_surface()
+
         self.enemy_name = enemy_name
 
         self.scale_factor = 4
@@ -31,13 +33,24 @@ class Enemy(pygame.sprite.Sprite):
         self.delta = Vector2()
         
         # Enemy properties
-        self.disable_pursue = False
-        self.triggered_delay = False
-        self.delay = 5
-        self.counter = 0
         self.facing_right = True
+        self.disable_pursue = False
+        self.finished_stun = False
+        self.delay = 2
+        self.counter = 0
         self.image = self.animations[int(self.animation_index)]
         self.rect = self.image.get_rect(topleft=pos)
+        self.mask = pygame.mask.from_surface(self.image)
+
+        # Enemy stats
+        self.initial_health = self.data['health']
+        self.health: int = self.data['health']
+        self.damage: int = self.data['damage']
+        self.knockback: int = self.data['knockback']
+
+        # Health Bar
+        self.health_bar_rect = pygame.Rect(*(self.rect.midtop - Vector2(0, 10)), self.rect.width * 8 / 10, self.rect.height / 6)
+        self.health_rect = self.health_bar_rect.copy()
 
     def import_assets(self):
         assert self.enemy_name in listdir('./assets/enemies/'), 'Enemy not found in assets'
@@ -47,7 +60,7 @@ class Enemy(pygame.sprite.Sprite):
         self.animations = [pygame.transform.scale(image:=pygame.image.load(f'./assets/enemies/{self.enemy_name}/{edited_enemy_name}_{i}.png').convert_alpha(), (image.get_width() * self.scale_factor, image.get_height() * self.scale_factor)) for i in range(1, 5)]
 
     def import_enemy_data(self):
-        with open('./src/enemy_stats.json') as rf:
+        with open('./data/enemy_data.json') as rf:
             self.data: dict = json.load(rf)[self.enemy_name]
         
     def check_collision_with_player(self, player):        
@@ -55,19 +68,27 @@ class Enemy(pygame.sprite.Sprite):
             print('collided')
 
     def persue_player(self, player):
-        self.delta = Vector2(self.rect.center).move_towards(player.rect.center, self.vel) - Vector2(self.rect.center)
+        if not self.disable_pursue:
+            if Vector2(player.rect.center).distance_to(self.rect.center) <= self.data['pursue radius']:
+                self.delta = Vector2(self.rect.center).move_towards(player.rect.center, self.vel) - Vector2(self.rect.center)
+            
+            else:
+                self.delta = Vector2()
     
     def trigger_delay(self):
-        if self.triggered_delay:
+        if self.disable_pursue:
             if self.counter > self.delay:
-                self.triggered_delay = False 
-                self.disable_persue = False
                 self.counter = 0
+                self.triggered_delay = False
+                self.disable_pursue = False
+                self.image = self.animations[int(self.animation_index)]
+            
             else:
                 self.counter += 0.1
                 self.delta = Vector2()
-
-
+                self.image = self.mask.to_surface()
+                self.image.set_colorkey((0, 0, 0))
+     
     def update_image(self):
         image = self.image
 
@@ -76,28 +97,48 @@ class Enemy(pygame.sprite.Sprite):
         else:
             self.image = pygame.transform.flip(image, True, False)
 
-    def update_direction(self):
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def draw_enemy(self, offset):
+        self.win.blit(self.image, self.rect.topleft - offset)
+
+    def update_direction(self, inverse_facing_right=False):
         if self.delta.x > 0:
-            self.facing_right = True 
+            self.facing_right = True if not inverse_facing_right else False
             self.direction.x = 1
-        else:
-            self.facing_right = False
+        elif self.delta.x < 0:
+            self.facing_right = False if not inverse_facing_right else True
             self.direction.x = -1
         
         if self.delta.y > 0:
             self.direction.y = 1
-        else:
+        elif self.delta.y < 0:
             self.direction.y = -1
 
-    def update(self, player: pygame.sprite.Sprite):
-        if Vector2(player.rect.center).distance_to(self.rect.center) <= self.data['pursue radius'] and not self.disable_pursue:
-            self.persue_player(player)
-            self.update_direction()
+    def update_health_bar(self, offset):
+        self.health_bar_rect.midtop = self.rect.midtop - offset - Vector2(0, 10)
+        self.health_rect = self.health_bar_rect.copy()
+        health_percentage = self.health / self.initial_health
+        self.health_rect.width = self.health_bar_rect.width * health_percentage
+
+        if health_percentage >= 0.6:
+            colour = COLOURS['green'] 
+        elif health_percentage >= 0.3:
+            colour = COLOURS['orange']
         else:
-            self.delta = Vector2()
+            colour = COLOURS['red']
+
+        pygame.draw.rect(self.win, 'black', self.health_bar_rect)        
+        pygame.draw.rect(self.win, colour, self.health_rect)
+        pygame.draw.rect(self.win, 'white', self.health_bar_rect, 2)
         
+    def update(self, player: pygame.sprite.Sprite, offset: Vector2):
+        self.persue_player(player)
         self.trigger_delay()
+        self.update_direction()
         self.update_image()
+        self.update_health_bar(offset)
+        self.draw_enemy(offset)
 
 
 
