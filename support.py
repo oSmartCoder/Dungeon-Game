3af1e8481809b929a50ced4e2c85cf9076f13333
oSmartCoder <92610222+oSmartCoder@users.noreply.github.com>
2024-04-21 00:29:28 +1000

    Create support.py

---
+++ support.py
@@ -0,0 +1,11 @@
+import pygame
+from pygame.math import Vector2
+
+FONT = './assets/fonts/font.ttf'
+
+def display_text(surface: pygame.Surface, text: str, pos: tuple[float, float], colour: tuple[int, int, int] | str = 'white', font_size: int = 30) -> None:
+    image = pygame.font.Font(FONT, font_size).render(text, True, colour)
+
+    surface.blit(image, image.get_rect(center=pos).topleft + Vector2(0, 4))
+
+
