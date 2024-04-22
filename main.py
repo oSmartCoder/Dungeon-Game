a88d6abbcd563dbbad0a9ffc431f2ca9ec043d37
oSmartCoder <92610222+oSmartCoder@users.noreply.github.com>
2024-04-22 23:20:19 +1000

    Update main.py

--- main.py
+++ main.py
@@ -14,11 +14,21 @@ class Main:
         self.CLOCK = pygame.time.Clock()
 
         self.win = pygame.display.set_mode((WIN_X, WIN_Y))
+        pygame.display.set_icon(pygame.image.load('./assets/gui/icon.png'))
+
+
+        # Custom cursor setup
+        self.cursor = pygame.image.load('./assets/gui/cursor/white.png')
 
         self.game = Game()
 
     def update(self):
         self.game.update()
+
+    def display_cursor(self):
+        pos = pygame.mouse.get_pos()
+
+        self.win.blit(self.cursor, pos)
 
     def run(self):
         while True:
