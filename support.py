ec33eca9d03fc2cff49260ce9ab813451903fbf2
oSmartCoder <92610222+oSmartCoder@users.noreply.github.com>
2024-04-23 23:03:49 +1000

    Update support.py

--- support.py
+++ support.py
@@ -1,11 +1,26 @@
 import pygame
 from pygame.math import Vector2
 
-FONT = './assets/fonts/font.ttf'
+from typing import NoReturn
 
-def display_text(surface: pygame.Surface, text: str, pos: tuple[float, float], colour: tuple[int, int, int] | str = 'white', font_size: int = 30) -> None:
-    image = pygame.font.Font(FONT, font_size).render(text, True, colour)
 
-    surface.blit(image, image.get_rect(center=pos).topleft)
+def display_text(surface: pygame.Surface, text: str, pos: tuple[float, float], colour: tuple[int, int, int] | str = 'white', font_name: str = 'font', font_size: int = 30, position: str = 'center') -> pygame.Rect | NoReturn:
+    font = pygame.font.Font(f'./assets/fonts/{font_name}.ttf', font_size)
+    image = font.render(text, True, colour)
+
+    match position:
+        case 'topleft':
+            rect = image.get_rect(topleft=pos)
+            surface.blit(image, rect)
+            return rect
+
+        case 'center':
+            rect = image.get_rect(center=pos)
+            surface.blit(image, rect)
+            return rect
+        
+        case _:
+            raise ValueError('Invalid position.')
+
 
 
