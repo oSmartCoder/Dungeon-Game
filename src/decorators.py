611822497a733f3cf0f34f7387bf67ad0e4bc20e
oSmartCoder <92610222+oSmartCoder@users.noreply.github.com>
2024-04-23 23:03:55 +1000

    Delete decorators.py

--- src/decorators.py
+++
@@ -1,9 +0,0 @@
-def execute_if(boolean: bool):
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            if boolean:
-                func(*args, **kwargs)
-            
-        return wrapper
-        
-    return decorator
